57d109a75c29aa53cf2acb1962d807ba5366e036
James Lin <james.sen.lin@icloud.com>
2025-11-05 17:44:22 -0500

    Added code

---
+++ main.py
@@ -0,0 +1,20 @@
+# Python Alarm Clock
+import time
+import datetime
+import pygame
+
+
+def set_alarm(alarm_time):
+    print(f"Alarm set for {alarm_time}")
+    sound_file = "alarm-327234.mp3"
+    is_running = True
+
+    while is_running:
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        print(current_time)
+
+        is_running = False
+
+if __name__ == "__main__":
+    alarm_time = input("Enter the alarm time (HH:MM:SS): ")
+    set_alarm(alarm_time)
