adcee16858f4fa44b5f954c46b4ba1054003e362
James Lin <james.sen.lin@icloud.com>
2025-11-06 00:35:26 -0500

    Finished code

--- main.py
+++ main.py
@@ -13,7 +13,19 @@ def set_alarm(alarm_time):
         current_time = datetime.datetime.now().strftime("%H:%M:%S")
         print(current_time)
 
-        is_running = False
+        if current_time == alarm_time:
+            print("WAKE UP")
+
+            pygame.mixer.init()
+            pygame.mixer.music.load(sound_file)
+            pygame.mixer.music.play()
+
+            while pygame.mixer.music.get_busy():
+                time.sleep(1)
+                
+            is_running = False
+
+        time.sleep(1)
 
 if __name__ == "__main__":
     alarm_time = input("Enter the alarm time (HH:MM:SS): ")
